f748c0b8f2912de2324a7e61aa076ac09eaa3e5f
Marc Sanz Drudis <93275620+MarcDrudis@users.noreply.github.com>
2024-04-17 23:41:07 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,11 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Copyright 2022 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_NAV_VI
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
