dc409ac7e90077e0927b5317ebaa7fa4bfb6af25
Marc Sanz Drudis <93275620+MarcDrudis@users.noreply.github.com>
2024-07-18 22:54:38 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,7 +9,7 @@
 #define MIRYOKU_EXTRA_COLEMAKDH
 #define MIRYOKU_NAV_VI
 
-#define MIRYOKU_ALTERNATIVES_MEDIA_VI \
+#define MIRYOKU_MEDIA \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         U_RGB_TOG,         \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        U_EP_TOG,          \
 U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       &u_out_tog,        \
